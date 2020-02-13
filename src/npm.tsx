187bd7aef262c204bcffb24424f69afa3d0a4cc4
Jay Fong <fjc0kb@gmail.com>
2020-02-13 17:48:13 +0800

    chore: tweaks

--- src/npm.tsx
+++ src/npm.tsx
@@ -1,9 +1,10 @@
 import _ from './npm.module.scss'
 import React, {useEffect, useMemo, useState} from 'react'
 import ReactDOM from 'react-dom'
-import {Card, Select, Tag, Tooltip, Typography} from 'antd'
+import {Button, Card, Select, Spin, Tag, Tooltip, Typography} from 'antd'
 import {detectPackageTypesState, PackageTypesState} from './api'
 import {getTypesPackageName} from './utils'
+import {GithubOutlined} from '@ant-design/icons'
 import {useStore} from './store'
 import {XBadge} from './components'
 
@@ -13,7 +14,7 @@ function Widget() {
     return state.clients.find(item => item.name === state.client)!
   }, [state.clients, state.client])
   const packageName = useMemo(() => {
-    return location.href.match(/package\/(@[^/]+\/[^/]+|[^/]+)/)![1]
+    return location.href.split('#')[0].match(/package\/(@[^/]+\/[^/]+|[^/]+)/)![1]
   }, [])
   const [packageTypesState, setPackageTypesState] = useState<PackageTypesState>(PackageTypesState.Pending)
 
@@ -21,78 +22,112 @@ function Widget() {
     detectPackageTypesState(packageName).then(setPackageTypesState)
   }, [])
 
-  return (
-    <div className={_.main}>
-      <h2 className={_.title}>{packageName}</h2>
-      <p>
-        <XBadge
-          label='types'
-          value={
-            packageTypesState === PackageTypesState.Pending
-              ? 'pending'
-              : packageTypesState === PackageTypesState.Missing
+  return packageTypesState === PackageTypesState.Pending
+    ? (
+      <div className={_.loading}>
+        <Spin spinning={true} />
+      </div>
+    )
+    : (
+      <div className={_.main}>
+        <h2 className={_.title}>{packageName}</h2>
+        <p>
+          <XBadge
+            label='types'
+            value={
+              packageTypesState === PackageTypesState.Missing
                 ? 'missing'
                 : packageTypesState === PackageTypesState.Included
                   ? 'included'
                   : getTypesPackageName(packageName)
-          }
-          color={
-            packageTypesState === PackageTypesState.Pending
-              ? 'lightgrey'
-              : packageTypesState === PackageTypesState.Missing
+            }
+            color={
+              packageTypesState === PackageTypesState.Missing
                 ? 'red'
                 : packageTypesState === PackageTypesState.Included
                   ? 'blue'
                   : 'green'
-          }
-        />
-      </p>
-      <Card
-        className={_.install}
-        title='Install'
-        size='small'
-        bordered={false}
-        extra={(
-          <Select
+            }
+          />
+        </p>
+        <p>
+          <Card
+            className={_.card}
+            title='Install'
             size='small'
-            value={state.client}
-            onChange={client => setState({client})}>
-            {state.clients.map(client => (
-              <Select.Option key={client.name} value={client.name}>
-                {client.name}
-              </Select.Option>
-            ))}
-          </Select>
-        )}>
-        <div className={`${_.item} ${_.code}`}>
-          <Typography.Text copyable={true}>
-            {`${client.name} ${client.installCommand} ${packageName}${
-              state.devDependency
-                ? ` ${client.devCommand}`
-                : ''
-            }${
-              packageTypesState === PackageTypesState.DefinitelyTyped && state.types
-                ? ` && ${client.name} ${client.installCommand} ${getTypesPackageName(packageName)} ${client.devCommand}`
-                : ''
-            }`}
-          </Typography.Text>
-        </div>
-        <div className={_.item}>
-          <Tooltip placement='bottomRight' title='Install as devDependency'>
-            <Tag.CheckableTag checked={state.devDependency} onChange={devDependency => setState({devDependency})}>D</Tag.CheckableTag>
-          </Tooltip>
-          {packageTypesState === PackageTypesState.DefinitelyTyped && (
-            <Tooltip placement='bottomRight' title='With types'>
-              <Tag.CheckableTag checked={state.types} onChange={types => setState({types})}>T</Tag.CheckableTag>
-            </Tooltip>
-          )}
-        </div>
-        {/* <div className={_.item}>
-          <a href={`https://unpkg.com/${packageName}/`} target='_blank'>Browse Files</a>
-        </div> */}
-      </Card>
-    </div>
-  )
+            bordered={false}
+            extra={(
+              <Select
+                size='small'
+                value={state.client}
+                onChange={client => setState({client})}>
+                {state.clients.map(client => (
+                  <Select.Option key={client.name} value={client.name}>
+                    {client.name}
+                  </Select.Option>
+                ))}
+              </Select>
+            )}>
+            <div className={`${_.item} ${_.code}`}>
+              <Typography.Text copyable={true}>
+                {`${client.name} ${client.installCommand} ${packageName}${
+                  state.devDependency
+                    ? ` ${client.devCommand}`
+                    : ''
+                }${
+                  packageTypesState === PackageTypesState.DefinitelyTyped && state.types
+                    ? ` && ${client.name} ${client.installCommand} ${getTypesPackageName(packageName)} ${client.devCommand}`
+                    : ''
+                }`}
+              </Typography.Text>
+            </div>
+            <div className={_.item}>
+              <Tooltip placement='bottomRight' title='Install as devDependency'>
+                <Tag.CheckableTag checked={state.devDependency} onChange={devDependency => setState({devDependency})}>D</Tag.CheckableTag>
+              </Tooltip>
+              {packageTypesState === PackageTypesState.DefinitelyTyped && (
+                <Tooltip placement='bottomRight' title='Install with types'>
+                  <Tag.CheckableTag checked={state.types} onChange={types => setState({types})}>T</Tag.CheckableTag>
+                </Tooltip>
+              )}
+            </div>
+          </Card>
+        </p>
+        <p>
+          <Card
+            className={_.card}
+            title='Explore'
+            size='small'
+            bordered={false}>
+            <div className={_.action}>
+              <Button
+                block={true}
+                type='primary'
+                onClick={() => {
+                  window.open(`https://unpkg.com/${packageName}/`, '_blank')
+                }}>
+                {'Browse files'}
+              </Button>
+            </div>
+            <div className={_.action}>
+              <Button
+                block={true}
+                type='primary'
+                onClick={() => {
+                  window.open(`https://unpkg.com/browse/${packageName}/package.json`, '_blank')
+                }}>
+                {'See package.json'}
+              </Button>
+            </div>
+          </Card>
+        </p>
+        <p className={_.github}>
+          <a href='https://github.com/fjc0k/npm-plus' target='_blank'>
+            <GithubOutlined />
+          </a>
+        </p>
+      </div>
+    )
 }
 
 // render
